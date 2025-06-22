d8d8dbcdde83baeb49d81d7e3151e8f6ba077473
Adeem <adeemakhtar@gmail.com>
2025-06-22 11:12:07 +0100

    Refactoring Calculator app using Class based implementation

--- main.py
+++ main.py
@@ -1,100 +1,110 @@
 # All Imports
 from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout
 
-# App Settings
-app = QApplication([])
-main_window = QWidget()
-main_window.setWindowTitle("Calculator App")
-main_window.resize(250,300)
-
-# All object/widgets
-text_box = QLineEdit()
-grid = QGridLayout()
-
-
-buttons = [
-    '7', '8', '9', '/', 
-    '4', '5', '6', '*', 
-    '1', '2', '3', '-', 
-    '0', '.', '=', '+'
-]
-clear = QPushButton('C')
-delete = QPushButton('<')
-
-# Functions
-
-def button_click():
+# Class CalculatorApp
+class CalculatorApp(QWidget):
     """
-        Handles the logic for calculator button clicks in a PyQt GUI application.
-
-        This function is triggered when any calculator button is clicked.
-        It performs actions based on the button's label:
-        
-        - If the button is '=', it evaluates the expression in the text box using `eval()` and displays the result.
-        If the expression is invalid, it displays an error message.
-        - If the button is 'C', it clears the text box.
-        - If the button is '<', it removes the last character from the current input.
-        - Otherwise, it appends the button's label (digit/operator) to the current input.
-
-        Assumes the use of a global or accessible `app.sender()` and a `text_box` object for UI interaction.
+        A simple PyQt5 calculator application window.
+        This class sets up the main window, initializes the display text box,
+        and prepares a grid layout for calculator buttons.
     """
-    button = app.sender()
-    text = button.text()
+    def __init__(self):
+        super().__init__()
 
-    if text == '=':
-        symbol = text_box.text()
+        # App Settings
+        self.setWindowTitle("Calculator App")
+        self.resize(250,300)
 
-        if len(symbol) > 0:
-            try:
-                res = eval(symbol)
-                text_box.setText(str(res))
+        # All object/widgets
+        self.text_box = QLineEdit()
+        self.grid = QGridLayout()
 
-            except Exception as e:
-                text_box.setText('Error:' + str(e))
-        else:
-            text_box.setText('0')
 
-    elif text == 'C':
-        text_box.clear()
+        self.buttons = [
+            '7', '8', '9', '/', 
+            '4', '5', '6', '*', 
+            '1', '2', '3', '-', 
+            '0', '.', '=', '+'
+        ]
 
-    elif text == '<':
-        current_value = text_box.text()
-        text_box.setText(current_value[:-1])
-
-    else:
-        current_value = text_box.text()
-        text_box.setText(current_value + text)
-
-# App Counters
-row = 0
-col = 0
-
-for text in buttons:
-    button = QPushButton(text)
-    button.clicked.connect(button_click)
-    grid.addWidget(button, row, col)
-
-    col += 1
-    if col > 3:
+        # App Counters
+        row = 0
         col = 0
-        row += 1
 
-# Design
-master_layout = QVBoxLayout()
-master_layout.addWidget(text_box)
-master_layout.addLayout(grid)
+        for text in self.buttons:
+            button = QPushButton(text)
+            button.clicked.connect(self.button_click)
+            self.grid.addWidget(button, row, col)
 
-button_row = QHBoxLayout()
-button_row.addWidget(clear)
-button_row.addWidget(delete)
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
 
+        self.clear = QPushButton('C')
+        self.delete = QPushButton('<')
 
-master_layout.addLayout(button_row)
-main_window.setLayout(master_layout)
+        # Design
+        master_layout = QVBoxLayout()
+        master_layout.addWidget(self.text_box)
+        master_layout.addLayout(self.grid)
 
-clear.clicked.connect(button_click)
-delete.clicked.connect(button_click)
+        button_row = QHBoxLayout()
+        button_row.addWidget(self.clear)
+        button_row.addWidget(self.delete)
+
+        master_layout.addLayout(button_row)
+        self.setLayout(master_layout)
+
+        self.clear.clicked.connect(self.button_click)
+        self.delete.clicked.connect(self.button_click)
+
+    # Functions
+    def button_click(self):
+        """
+            Handles the logic for calculator button clicks in a PyQt GUI application.
+
+            This function is triggered when any calculator button is clicked.
+            It performs actions based on the button's label:
+            
+            - If the button is '=', it evaluates the expression in the text box using `eval()` and displays the result.
+            If the expression is invalid, it displays an error message.
+            - If the button is 'C', it clears the text box.
+            - If the button is '<', it removes the last character from the current input.
+            - Otherwise, it appends the button's label (digit/operator) to the current input.
+
+            Assumes the use of a global or accessible `app.sender()` and a `text_box` object for UI interaction.
+        """
+        button = app.sender()
+        text = button.text()
+
+        if text == '=':
+            symbol = self.text_box.text()
+
+            if len(symbol) > 0:
+                try:
+                    res = eval(symbol)
+                    self.text_box.setText(str(res))
+
+                except Exception as e:
+                    self.text_box.setText('Error:' + str(e))
+            else:
+                self.text_box.setText('0')
+
+        elif text == 'C':
+            self.text_box.clear()
+
+        elif text == '<':
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value[:-1])
+
+        else:
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value + text)
 
 # Show/Run
-main_window.show()
-app.exec_()
+if __name__ in "__main__":
+    app = QApplication([])
+    main_window = CalculatorApp()
+    main_window.show()
+    app.exec_()
